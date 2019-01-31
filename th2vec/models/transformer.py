4cff9311de419d947f24407ab950347f133b54bf
Stanislas Polu <polu.stanislas@gmail.com>
2019-01-31 12:44:16 +0000

    dropout instead of bn, 2 transformer

--- th2vec/models/transformer.py
+++ th2vec/models/transformer.py
@@ -54,18 +54,21 @@ class P(nn.Module):
 
         self.inner_cnj = nn.Sequential(*[
             nn.Linear(self.hidden_size, self.hidden_size, bias=False),
-            nn.BatchNorm1d(self.hidden_size),
+            # nn.BatchNorm1d(self.hidden_size),
+            nn.Dropout(0.1),
             nn.ReLU(),
         ])
         self.inner_thr = nn.Sequential(*[
             nn.Linear(self.hidden_size, self.hidden_size, bias=False),
-            nn.BatchNorm1d(self.hidden_size),
+            # nn.BatchNorm1d(self.hidden_size),
+            nn.Dropout(0.1),
             nn.ReLU(),
         ])
 
         self.head = nn.Sequential(*[
             nn.Linear(2*self.hidden_size, self.hidden_size, bias=False),
-            nn.BatchNorm1d(self.hidden_size),
+            # nn.BatchNorm1d(self.hidden_size),
+            nn.Dropout(0.1),
             nn.ReLU(),
             nn.Linear(self.hidden_size, 1),
             nn.Sigmoid(),
