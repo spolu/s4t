2c7d985d0a178c11b366a58c7c85458eddc00945
Stanislas Polu <polu.stanislas@gmail.com>
2019-02-13 14:57:04 +0000

    fix naming

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
             'sat_train_solver=sat.solver.solver:train',
             'sat_test_solver=sat.solver.solver:test',
             'holstep_preprocess=dataset.holstep:preprocess',
-            'th2vec_train_diret_premiser=th2vec.direct_premiser:train',
+            'th2vec_train_direct_premiser=th2vec.direct_premiser:train',
             'th2vec_train_premise_embedder=th2vec.premise_embedder:train',
             'th2vec_train_autoencoder_embedder=th2vec.autoencoder_embedder:train',
             'th2vec_train_premiser=th2vec.premiser:train',
