96e124f9933f07a2b54269b82bb0129adb7babf7
Stanislas Polu <polu.stanislas@gmail.com>
2019-02-28 07:04:43 +0000

    beef up lstm model

--- prooftrace/models/lstm.py
+++ prooftrace/models/lstm.py
@@ -19,13 +19,21 @@ class P(nn.Module):
             config.get('prooftrace_sequence_length')
         self.hidden_size = \
             config.get('prooftrace_hidden_size')
+        self.lstm_hidden_size = \
+            config.get('prooftrace_lstm_hidden_size')
+        self.lstm_layer_count = \
+            config.get('prooftrace_lstm_layer_count')
 
         self.embedder = ActionEmbedder(config)
         self.embedder.to(self.device)
 
+        self.position_embedding = nn.Embedding(
+            self.sequence_length, self.hidden_size
+        )
+
         self.lstm = nn.LSTM(
-            self.hidden_size, self.hidden_size,
-            num_layers=2, batch_first=True,
+            self.hidden_size, self.lstm_hidden_size,
+            num_layers=self.lstm_layer_count, batch_first=True,
         )
 
         # position_decoder = nn.Linear(
@@ -34,18 +42,18 @@ class P(nn.Module):
         # position_decoder.weight = self.position_embedding.weight
 
         self.action_head = nn.Sequential(
-            nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Linear(self.hidden_size, len(ACTION_TOKENS)),
+            nn.Linear(self.lstm_hidden_size, self.lstm_hidden_size),
+            nn.Linear(self.lstm_hidden_size, len(ACTION_TOKENS)),
             nn.LogSoftmax(dim=1),
         )
         self.left_head = nn.Sequential(
-            nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Linear(self.hidden_size, self.sequence_length),
+            nn.Linear(self.lstm_hidden_size, self.lstm_hidden_size),
+            nn.Linear(self.lstm_hidden_size, self.sequence_length),
             nn.LogSoftmax(dim=1),
         )
         self.right_head = nn.Sequential(
-            nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Linear(self.hidden_size, self.sequence_length),
+            nn.Linear(self.lstm_hidden_size, self.lstm_hidden_size),
+            nn.Linear(self.lstm_hidden_size, self.sequence_length),
             nn.LogSoftmax(dim=1),
         )
 
@@ -74,6 +82,14 @@ class P(nn.Module):
             self,
             embeds,
     ):
-        hiddens, _ = self.lstm(embeds)
+        pos_embeds = torch.arange(
+            self.sequence_length, dtype=torch.long
+        ).to(self.device)
+        pos_embeds = pos_embeds.unsqueeze(0).expand(
+           embeds.size(0), self.sequence_length,
+        )
+        pos_embeds = self.position_embedding(pos_embeds)
+
+        hiddens, _ = self.lstm(embeds + pos_embeds)
 
         return hiddens
