254e25b83666bf5b42ad0bcff8db45f24796ed37
Stanislas Polu <polu.stanislas@gmail.com>
2019-05-03 12:57:47 +0000

    move surgeries to archive

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'run_surgery_20190401_ppo_iota_separate_hiddens=surgeries.20190401_ppo_iota_separate_hiddens:run',
-            'verify_surgery_20190401_ppo_iota_separate_hiddens=surgeries.20190401_ppo_iota_separate_hiddens:verify',
-
             'prooftrace_extract=prooftrace.prooftrace:extract',
             'prooftrace_dump_shared=prooftrace.prooftrace:dump_shared',
 
@@ -26,12 +23,8 @@ setup(
             'prooftrace_test_fusion=prooftrace.repl.fusion:test',
             'prooftrace_test_repl_env=prooftrace.repl.env:test',
 
-            'prooftrace_train_language_model=prooftrace.language_model:train',
-            'prooftrace_search_language_model=prooftrace.language_model_search:search',
             'prooftrace_ppo_syn_run=prooftrace.ppo_iota:syn_run',
             'prooftrace_ppo_ack_run=prooftrace.ppo_iota:ack_run',
-            'prooftrace_language_model_syn_run=prooftrace.language_model_iota:syn_run',
-            'prooftrace_language_model_ack_run=prooftrace.language_model_iota:ack_run',
             'prooftrace_embeds_tsne_extract=prooftrace.embeds.tsne:extract',
             'prooftrace_embeds_viewer=prooftrace.embeds.viewer.viewer:run',
         ],
