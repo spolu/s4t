13c699e23d56985c186000f19d83159dcd942226
Stanislas Polu <polu.stanislas@gmail.com>
2019-05-13 11:49:15 +0000

    shuffle

--- prooftrace/language_model_iota.py
+++ prooftrace/language_model_iota.py
@@ -52,8 +52,7 @@ class ACK:
         self._train_loader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=self._config.get('prooftrace_lm_batch_size'),
-            shuffle=None,
-            sampler=None,
+            shuffle=True,
             collate_fn=lm_collate,
             num_workers=8,
         )
