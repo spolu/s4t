8c066c5c3d29c6b9c068441a68c9f1cb610c0243
Stanislas Polu <polu.stanislas@gmail.com>
2019-05-14 15:28:33 +0000

    re-unify value and lm

--- setup.py
+++ setup.py
@@ -24,11 +24,8 @@ setup(
             'prooftrace_test_fusion=prooftrace.repl.fusion:test',
             'prooftrace_test_repl_env=prooftrace.repl.env:test',
 
-            'prooftrace_train_lm=prooftrace.language_model:train',
             'prooftrace_lm_syn_run=prooftrace.language_model_iota:syn_run',
             'prooftrace_lm_ack_run=prooftrace.language_model_iota:ack_run',
-            'prooftrace_val_syn_run=prooftrace.value_iota:syn_run',
-            'prooftrace_val_ack_run=prooftrace.value_iota:ack_run',
             'prooftrace_tree_search=prooftrace.tree_search:mcts',
 
             'prooftrace_ppo_syn_run=prooftrace.ppo_iota:syn_run',
