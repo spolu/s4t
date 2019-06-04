258262535345b4ef1af11f160ac4bd2d1a744f12
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-04 19:44:34 +0000

    iterate on scripts

---
+++ scripts/prooftrace_search_ack_shutdown.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+killall /home/stan/.conda/envs/z3ta/bin/python
--- scripts/prooftrace_search_ack_startup_stan.sh
+++ scripts/prooftrace_search_ack_startup_stan.sh
@@ -7,3 +7,25 @@ source /opt/anaconda3/bin/activate z3ta
 
 git pull origin master
 python setup.py develop
+
+tmux -S ~/ack new-session -d -s ack
+
+/home/stan/src/z3ta/ack++ search 1 0
+/home/stan/src/z3ta/ack++ search 1 1
+/home/stan/src/z3ta/ack++ search 1 2
+/home/stan/src/z3ta/ack++ search 1 3
+
+/home/stan/src/z3ta/ack++ search 1 0
+/home/stan/src/z3ta/ack++ search 1 1
+/home/stan/src/z3ta/ack++ search 1 2
+/home/stan/src/z3ta/ack++ search 1 3
+
+/home/stan/src/z3ta/ack++ search 1 0
+/home/stan/src/z3ta/ack++ search 1 1
+/home/stan/src/z3ta/ack++ search 1 2
+/home/stan/src/z3ta/ack++ search 1 3
+
+/home/stan/src/z3ta/ack++ search 1 0
+/home/stan/src/z3ta/ack++ search 1 1
+/home/stan/src/z3ta/ack++ search 1 2
+/home/stan/src/z3ta/ack++ search 1 3
