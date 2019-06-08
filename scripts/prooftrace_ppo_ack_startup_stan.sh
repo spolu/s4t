02087a4765e733e7bb22df24ddc35eac7dd6121e
Stanislas Polu <polu.stanislas@gmail.com>
2019-06-08 13:56:33 +0000

    bump to 5 worker per GPU

--- scripts/prooftrace_ppo_ack_startup_stan.sh
+++ scripts/prooftrace_ppo_ack_startup_stan.sh
@@ -29,3 +29,8 @@ tmux -S ~/ppo_ack new-session -d -s ppo_ack
 /home/stan/src/z3ta/scripts/ppo_ack++ 1 1
 /home/stan/src/z3ta/scripts/ppo_ack++ 1 2
 /home/stan/src/z3ta/scripts/ppo_ack++ 1 3
+
+/home/stan/src/z3ta/scripts/ppo_ack++ 1 0
+/home/stan/src/z3ta/scripts/ppo_ack++ 1 1
+/home/stan/src/z3ta/scripts/ppo_ack++ 1 2
+/home/stan/src/z3ta/scripts/ppo_ack++ 1 3
