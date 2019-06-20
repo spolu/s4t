30b0cf6dec492f46d22c5bf7c65d4152abc12ea4
Stanislas Polu <polu.stanislas@gmail.com>
2019-06-20 12:03:32 +0000

    reduce number of workers

--- scripts/prooftrace_lm_rollout_wrk_startup_stan.sh
+++ scripts/prooftrace_lm_rollout_wrk_startup_stan.sh
@@ -24,8 +24,3 @@ tmux -S ~/lm_rollout_wrk new-session -d -s lm_rollout_wrk
 /home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 1
 /home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 2
 /home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 3
-
-/home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 0
-/home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 1
-/home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 2
-/home/stan/src/z3ta/scripts/lm_rollout_wrk++ 1 3
