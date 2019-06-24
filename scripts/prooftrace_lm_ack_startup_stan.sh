8494a2496719e2dbdeb3ef4fa1fd471efd7df20d
Stanislas Polu <polu.stanislas@gmail.com>
2019-06-24 10:44:30 +0000

    bump number of ack workers

--- scripts/prooftrace_lm_ack_startup_stan.sh
+++ scripts/prooftrace_lm_ack_startup_stan.sh
@@ -20,7 +20,7 @@ tmux -S ~/lm_ack new-session -d -s lm_ack
 /home/stan/src/z3ta/scripts/lm_ack++ 1 2
 /home/stan/src/z3ta/scripts/lm_ack++ 1 3
 
-# /home/stan/src/z3ta/scripts/lm_ack++ 1 0
-# /home/stan/src/z3ta/scripts/lm_ack++ 1 1
-# /home/stan/src/z3ta/scripts/lm_ack++ 1 2
-# /home/stan/src/z3ta/scripts/lm_ack++ 1 3
+/home/stan/src/z3ta/scripts/lm_ack++ 1 0
+/home/stan/src/z3ta/scripts/lm_ack++ 1 1
+/home/stan/src/z3ta/scripts/lm_ack++ 1 2
+/home/stan/src/z3ta/scripts/lm_ack++ 1 3
