d4927f6067172783cb53660af34fe3feb2142fa7
Stanislas Polu <polu.stanislas@gmail.com>
2019-06-29 09:23:41 +0000

    update aws_syn URL and double batch size

--- scripts/prooftrace_lm_ack_startup_stan_aws.sh
+++ scripts/prooftrace_lm_ack_startup_stan_aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sshfs stan@ec2-18-222-144-196.us-east-2.compute.amazonaws.com:/home/stan/tmp/iota ~/tmp/iota
+sshfs stan@ec2-13-58-204-144.us-east-2.compute.amazonaws.com
 
 cd ~/src/z3ta
 source ~/opt/miniconda3/bin/activate z3ta
@@ -9,15 +9,6 @@ git pull origin master
 python setup.py develop
 
 tmux -S ~/lm_ack new-session -d -s lm_ack
-
-/home/stan/src/z3ta/scripts/lm_ack++ 1 0
-/home/stan/src/z3ta/scripts/lm_ack++ 1 1
-/home/stan/src/z3ta/scripts/lm_ack++ 1 2
-/home/stan/src/z3ta/scripts/lm_ack++ 1 3
-/home/stan/src/z3ta/scripts/lm_ack++ 1 4
-/home/stan/src/z3ta/scripts/lm_ack++ 1 5
-/home/stan/src/z3ta/scripts/lm_ack++ 1 6
-/home/stan/src/z3ta/scripts/lm_ack++ 1 7
 
 /home/stan/src/z3ta/scripts/lm_ack++ 1 0
 /home/stan/src/z3ta/scripts/lm_ack++ 1 1
